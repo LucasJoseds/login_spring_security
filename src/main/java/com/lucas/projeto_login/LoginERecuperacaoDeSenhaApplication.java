3267ea5c542be809a280ae33313019e54d47b853
Lucas Jose da Silva <07055338166@tocantins.governo>
2024-07-02 17:48:34 -0300

    first commit

---
+++ src/main/java/com/lucas/projeto_login/LoginERecuperacaoDeSenhaApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.projeto_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginERecuperacaoDeSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginERecuperacaoDeSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/projeto_login/LoginERecuperacaoDeSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.projeto_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginERecuperacaoDeSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
